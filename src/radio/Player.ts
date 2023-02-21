93feb39ac33d65ac2711d2a7b2c977b395e8bf8b
Abdul Rahim <abdulrahimshoid@gmail.com>
2023-02-21 12:26:01 +0000

    update: RadioPlayer

--- src/radio/Player.ts
+++ src/radio/Player.ts
@@ -20,16 +20,32 @@ interface RadioOptions {
 }
 
 export class RadioPlayer {
-	private connection: VoiceConnection | null;
-	private player: AudioPlayer | null;
-	private data: Station | null;
-	private resource: AudioResource | null;
+	public connection: VoiceConnection | null;
+	public player: AudioPlayer | null;
+	public resource: AudioResource<Station>;
+	public data: Station | null;
 
-	public constructor() {
+	public constructor(connection: VoiceConnection, resource: AudioResource<Station>, data: Station) {
 		this.player = createAudioPlayer();
-		this.connection = null;
-		this.resource = null;
-		this.data = null;
+		this.connection = connection;
+		this.resource = resource;
+		this.data = data;
+
+		this.connection?.on(VoiceConnectionStatus.Disconnected, async (_oldState, _newState) => {
+			try {
+				await Promise.race([
+					entersState(this.connection!, VoiceConnectionStatus.Signalling, 5_000),
+					entersState(this.connection!, VoiceConnectionStatus.Connecting, 5_000)
+				]);
+			} catch (error) {
+				this.connection?.destroy();
+				this.player?.stop();
+			}
+		});
+
+		this.player.on(AudioPlayerStatus.Playing, () => {
+			console.log('The audio player has started playing!');
+		});
 	}
 
 	/**
@@ -42,7 +58,7 @@ export class RadioPlayer {
 		if (this.playing) {
 			this.player?.stop();
 			this.resource = createAudioResource(data.url_resolved as Readable, { inputType: StreamType.Arbitrary });
-			await this.playResource(data, this.resource);
+			this.playResource(data, this.resource);
 		}
 
 		if (!this.connection) await this.connect(options.voice);
@@ -51,7 +67,12 @@ export class RadioPlayer {
 		if (!data) throw new Error('No radio stations found!');
 
 		this.resource = createAudioResource(data.url_resolved as Readable, { inputType: StreamType.Arbitrary });
-		await this.playResource(data, this.resource);
+		return this.playResource(data, this.resource);
+	}
+
+	public async search(query: string) {
+		const data = await RadioBrowser.searchStations({ name: query }).then((data) => data[0]);
+		return data.url_resolved;
 	}
 
 	/**
@@ -66,7 +87,7 @@ export class RadioPlayer {
 		});
 
 		try {
-			await entersState(this.connection, VoiceConnectionStatus.Ready, 30000);
+			return entersState(this.connection, VoiceConnectionStatus.Ready, 30000);
 		} catch (error) {
 			this.connection.destroy();
 			throw error;
@@ -79,49 +100,18 @@ export class RadioPlayer {
 	public async disconnect() {
 		if (this.connection) {
 			await entersState(this.connection, VoiceConnectionStatus.Destroyed, 5000);
-			this.connection = null;
+			this.connection.destroy();
 			this.player?.stop();
 			this.data = null;
 		}
 	}
 
-	// /**
-	//  * Pauses the current resource
-	//  * @returns Boolean of the resource pause state
-	//  */
-	// public pause() {
-	// 	return this.player?.pause() as boolean;
-	// }
-
-	// /**
-	//  * Unpauses the current resource
-	//  * @returns Boolean of the resource unpause state
-	//  */
-	// public unpause() {
-	// 	return this.player?.unpause() as boolean;
-	// }
-
-	// /**
-	//  * Sets the volume for the current resource
-	//  * @param {Number} volume The volume to set
-	//  */
-	// public setVolume(volume: number) {
-	// 	return this.resource?.volume?.setVolumeLogarithmic(volume / 100);
-	// }
-
 	/**
 	 * @returns The radio data
 	 */
-	public current(): Station | null {
+	public current() {
 		return this.data;
 	}
-
-	// /**
-	//  * @returns The current resource volume
-	//  */
-	// public get volume() {
-	// 	return this.resource?.volume?.volume;
-	// }
 
 	/**
 	 * @returns True or false, depending on if the player is playing
@@ -131,37 +121,10 @@ export class RadioPlayer {
 		return false;
 	}
 
-	private async playResource(data, resource: AudioResource) {
+	private playResource(data, resource: AudioResource) {
 		this.player?.play(resource);
-
-		this.data = data;
 		this.connection?.subscribe(this.player!);
-
-		if (this.connection?.listeners.name !== VoiceConnectionStatus.Disconnected) {
-			this.connection?.on(VoiceConnectionStatus.Disconnected, async (_oldState, _newState) => {
-				try {
-					await Promise.race([
-						entersState(this.connection!, VoiceConnectionStatus.Signalling, 5_000),
-						entersState(this.connection!, VoiceConnectionStatus.Connecting, 5_000)
-					]);
-				} catch (error) {
-					this.connection?.destroy();
-					this.connection = null;
-					this.player?.stop();
-				}
-			});
-
-			await entersState(this.player!, AudioPlayerStatus.Playing, 5000);
-		}
-
-		if (this.connection?.listeners.name !== VoiceConnectionStatus.Destroyed) {
-			this.connection?.on(VoiceConnectionStatus.Destroyed, (_oldState, _newState) => {
-				this.connection?.destroy();
-				this.connection = null;
-				this.player?.stop();
-			});
-		}
-
+		this.data = data;
 		return this.data;
 	}
 }
